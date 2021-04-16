d7b66628002b1dd1f985620c0005bf28feef6510
tanggaomeng <2647437068@qq.com>
2021-04-16 14:24:12 +0800

    测试

---
+++ HDFSDemo/src/main/java/com/learning/Test.java
@@ -0,0 +1,13 @@
+package com.learning;
+
+/**
+ * @Author: tanggaomeng
+ * @Date: 2021/4/16 14:22
+ * @Description:
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
