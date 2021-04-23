a095049cc6f9efce2268e228d2d180cd143883b0
tanggaomeng <2647437068@qq.com>
2021-04-23 15:34:29 +0800

    MapReduce Demo

--- MapReduceDemo/src/main/java/com/learning/nokerberos/mapreduce/sgg1wordcount/WordCountMapper.java
+++ MapReduceDemo/src/main/java/com/learning/nokerberos/mapreduce/sgg1wordcount/WordCountMapper.java
@@ -1,4 +1,4 @@
-package com.learning.nokerberos.mapreduce.wordcount1;
+package com.learning.nokerberos.mapreduce.sgg1wordcount;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
@@ -9,21 +9,34 @@ import java.io.IOException;
 
 /**
  * @Author: tanggaomeng
- * @Date: 2021/4/22 9:39
+ * @Date: 2021/4/23 15:09
  * @Description:
  * @Version 1.0
+ * <p>
+ * KEYIN, map阶段输入的key的类型：LongWritable
+ * VALUEIN, map阶段输入的value的类型：Text
+ * KEYOUT, map阶段输出的key的类型： Text
+ * VALUEOUT, map阶段输出的value的类型： IntWritable
  */
 public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
-    // Ctrl + o 重写父类方法
-    // key: 偏移量
-    // value: 值
+    private final Text outK = new Text();
+    private final IntWritable outV = new IntWritable(1);
+
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         //super.map(key, value, context);
+        // 1.获取一行
         String line = value.toString();
+
+        // 2.切割
         String[] words = line.split(" ");
+
+        // 3.循环写出
         for (String word : words) {
-            context.write(new Text(word), new IntWritable(1));
+            // 封装 outK
+            outK.set(word);
+            // 写出
+            context.write(outK, outV);
         }
 
     }
